bcc4052e129befbafadca85857a7f43d9bc00579
A.K. Johnson <nkdluck@gmail.com>
2024-07-28 18:53:28 -0500

    Create Tree.java - Learn Java @ Codecademy
    First practice project for Learn Java at Codecademy by Audrey Johnson in July 2024.
    This one practices applying the main method and system printouts.

---
+++ Tree.java
@@ -0,0 +1,23 @@
+/*
+First practice project for Learn Java at Codecademy by Audrey Johnson in July 2024.
+This one practices applying the main method and system printouts. 
+*/
+
+public class Tree {
+  public static void main(String[] args) {
+    /*We're going to introduce ourselves and print a tree to the screen
+    */
+    System.out.println("Hey there, I'm Audrey and I'm learning to code in Java!");
+    System.out.println("I'm going to plant a tree today!");
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  ");
+    System.out.println(" * ** * ");
+    System.out.println(" ****** ");
+    System.out.println("  **** ");
+    System.out.println("   **  ");
+    System.out.println("   **  ");   
+  }
+}
